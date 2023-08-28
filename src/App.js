8c7bdd5bb927884f1ef7e5a77824f864c1a13d6f
Rysth <johnpalacios.t@gmail.com>
2023-08-28 13:59:26 -0500

    Added Poppins font family and Normalize.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={() => 'Hi'} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import planet from '../../assets/img/planet.png';
 import './Header.css';
 
@@ -6,9 +7,18 @@ function Header() {
     <div className="nav">
       <div className="container">
         <div className="logo">
-          <img className="image" src={planet} alt="logo" />
-          <h1>Space Traveler&lsquo;s Hub</h1>
+          <img className="nav-image" src={planet} alt="logo" />
+          <h1 className="nav-title">Space Travelers&lsquo; Hub</h1>
         </div>
+        <nav className="navbar">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <NavLink to="/profile" className="nav-link">
+                My Profile
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
