48847b4424dfcc501d73b33ae959475ec61c1624
Rysth <johnpalacios.t@gmail.com>
2023-08-29 12:19:05 -0500

    Fetched missions using the SpaceX API.

---
+++ src/redux/missions/missionsSlice.js
@@ -0,0 +1,57 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchMissions = createAsyncThunk(
+  'missions/fetchMissions',
+  async () => {
+    try {
+      const response = await fetch('https://api.spacexdata.com/v3/missions');
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return error;
+    }
+  },
+);
+
+const initialState = {
+  missions: [],
+  isLoading: false,
+  hasError: false,
+};
+
+const missionsSlice = createSlice({
+  name: 'missions',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => {
+        state.isLoading = true;
+        state.hasError = false;
+      })
+
+      .addCase(fetchMissions.fulfilled, (state, action) => {
+        state.isLoading = true;
+        state.hasError = false;
+        if (state.missions.length === 0) {
+          state.missions = action.payload.map((mission) => {
+            const myMission = {
+              id: mission.mission_id,
+              name: mission.mission_name,
+              description: mission.description,
+            };
+            return myMission;
+          });
+        }
+      })
+
+      .addCase(fetchMissions.rejected, (state) => {
+        state.isLoading = false;
+        state.hasError = true;
+      });
+  },
+});
+
+export const missionsActions = missionsSlice.actions;
+
+export default missionsSlice.reducer;
