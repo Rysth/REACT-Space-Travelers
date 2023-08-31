20f7f36ad71ae69f954d64c44007bdd99aa4f5c6
MHNazary <mhnazary2020@gmail.com>
2023-08-31 21:40:17 +0430

    Added new change to Display_Rocket.js

--- src/components/Profile/Display_Rocket.js
+++ src/components/Profile/Display_Rocket.js
@@ -1,21 +1,20 @@
 import '../../index.css';
 import { useSelector } from 'react-redux';
 
-// profile rockets component
 function ProfRockets() {
-  const { rocket } = useSelector((store) => store.rockets);
-  const bookRocket = rocket.filter((rocket) => rocket.isReserved);
+  const { rockets } = useSelector((store) => store.rockets);
+  const bookRocket = rockets.filter((rocket) => rocket.isReserved);
   if (bookRocket.length === 0) {
     return <div className="border border-gray-600 px-4 py-5 font-thin text-base">You have not reserved any rocket yet.</div>;
   }
   return (
-    <div>
+    <ul className="list-group">
       {bookRocket.map((rocket) => (
-        <div key={rocket.id} className="showRocket border border-gray-600 px-4 py-5 font-thin text-base">
+        <li key={rocket.id} className="showRocket border border-gray-600 px-4 py-5 font-thin text-base">
           {rocket.name}
-        </div>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
 
