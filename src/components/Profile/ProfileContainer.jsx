58d9361c2871ea269226c69fe2dd5313f5ef52cc
MHNazary <mhnazary2020@gmail.com>
2023-08-31 20:58:42 +0430

    profileContainer added to project

---
+++ src/components/Profile/ProfileContainer.jsx
@@ -0,0 +1,16 @@
+import '../../index.css';
+
+function ProfileContainer() {
+  return (
+    <section className="flex px-16 mt-6">
+      <div className="w-1/2 font-bold text-xl font-serif">
+        <h1>My Mission</h1>
+      </div>
+      <div className="w-1/2 font-bold text-xl font-serif">
+        <h1>My Profile</h1>
+      </div>
+    </section>
+  );
+}
+
+export default ProfileContainer;
--- src/components/Route/Rout.js
+++ src/components/Route/Rout.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router';
 import RocketsContainer from '../Rockets/RocketsContainer';
 import Missions from '../Missions/Missions';
+import ProfileContainer from '../Profile/ProfileContainer';
 
 function Rout() {
   return (
     <Routes>
       <Route path="/" element={<RocketsContainer />} />
       <Route path="/missions" element={<Missions />} />
-      <Route path="/profile" element={<h2>My Profile</h2>} />
+      <Route path="/profile" element={<ProfileContainer />} />
     </Routes>
   );
 }
